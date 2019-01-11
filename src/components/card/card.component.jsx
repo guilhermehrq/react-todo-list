c5bfe365b10c5b1fae38d89aaad6957697e7b8f5
guilhermehrq <guilherme_hrq99@outlook.com>
2019-01-11 08:18:58 -0200

    iniciando tela de tarefas

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -2,9 +2,35 @@ import React, { Component } from 'react';
 import './card.component.scss';
 
 export class Card extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            size: this.props.size
+        }
+        this.sizes = [480, 600, 840, 960, 1280, 1440, 1600]
+
+        this.verifySize = this.verifySize.bind(this);
+    }
+
+    componentDidMount() {
+        this.verifySize();
+    }
+
+    verifySize() {
+        const size = this.state.size;
+
+        if(size && !this.sizes.includes(+size)) {
+            console.warn('Valor de "size" inválido!');
+            this.setState({ ...this.state, size: '' });
+        } else if(size) {
+            this.setState({ ...this.state, size: ` size-${ size }` });
+        }
+    }
+
     render() {
         return (
-            <div className={`ui-card size-${ this.props.size || 480 }`}>
+            <div className={`ui-card${ this.state.size || '' }`}>
                 { this.props.children }
             </div>
         )
--- src/views/todo/todo.jsx
+++ src/views/todo/todo.jsx
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
 import Card from '../../components/card/card.component';
 
+import './todo.scss';
+
 export default class Todo extends Component {
     render() {
         return (
-            <Card size={ 600 }>
-                <h1 className='ui-card-title'>
-                    Teste
-                </h1>
-            </Card>
+            <div className='ui-s600' style={{ marginTop: '32px' }}>
+                <Card className='tools-card'>
+                    <div className='ui-card-title title-container'>
+                        <span>Lista de tarefas</span>
+
+                        <div>
+                            <i className='material-icons'>filter_list</i>
+                        </div>
+                    </div>
+                    
+                    <div className='ui-card-content filter-container'>
+                        <span>batata</span>
+                    </div>
+                </Card>
+            </div>
         )
     }
 }
